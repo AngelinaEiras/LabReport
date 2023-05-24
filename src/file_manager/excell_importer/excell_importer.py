ca8c0626757a83fc488227aeee0deb2bf1fc8e16
Afonso Sousa <soa5brg@bosch.com>
2023-05-24 09:36:00 +0100

    add main.py and python requirements file

--- src/file_manager/excell_importer/excell_importer.py
+++ src/file_manager/excell_importer/excell_importer.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from pandas import DataFrame
 from typing import Optional
-import csv
-from dataclasses import dataclass
+from pydantic.dataclasses import dataclass
+
+from openpyxl import load_workbook, worksheet, workbook
 
 @dataclass
 class BoardReading:
@@ -20,19 +21,25 @@ class ExcellImporter:
         line = self.__find_starting_line()
         
         while(self.__find_next_table()):
-            self.__load_board_readings
+            self.__load_board_readings()
             
 
 
     def __open_file(self,path:str, sheet_name:str):
 
-        ### TEMP ###
-        path = "/home/angelina/Desktop/dissertacao/a_laboratório/PB/20230224_triton_VVB.xlsx"
-        sheet_name = "Plate 1 - Sheet1 (2)"
-        ############
-        
-        excell_file = pd.ExcelFile(path)
-        self.csv_file = pd.read_excel(excell_file, sheet_name=sheet_name).to_csv (sheet_name+".csv", index = None, header=True)
+### TEMP ###
+path = "tests/artifacts/20230224_triton_VVB.xlsx"
+sheet_name = "Plate 1 - Sheet1 (2)"
+############
+
+# excell_file = pd.ExcelFile(path)
+# self.csv_file = pd.read_excel(excell_file, sheet_name=sheet_name).to_csv (sheet_name+".csv", index = None, header=True)
+wb = load_workbook(filename = path)
+ws = wb.get_sheet_by_name(sheet_name)
+print(ws.dimensions)
+
+
+
 
     def __find_starting_line(self):
         while(True):pass
@@ -41,7 +48,5 @@ class ExcellImporter:
     def __find_next_table(self):
         pass
 
-
-    @classmethod
-    def load_board_reading():
+    def load_board_reading(self):
         pass
