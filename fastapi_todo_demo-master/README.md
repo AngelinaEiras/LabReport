9c87db10f455ceb0782a7fc69eddc86b6c9b1533
AngelinaEiras <angie.eiras@gmail.com>
2023-07-05 09:19:38 +0100

    testar app encontrada na net desejar boa sorte

---
+++ fastapi_todo_demo-master/README.md
@@ -0,0 +1,9 @@
+# fastapi todo add demo based on this article: https://www.toptal.com/python/build-high-performing-apps-with-the-python-fastapi-framework
+## Install Env
+```bash
+poetry install
+poetry shell
+```
+
+## Run tests
+`tox`
--- src/file_manager/tests/multiples/HTMX_/main.py
+++ src/file_manager/tests/multiples/HTMX_/main.py
@@ -46,13 +46,14 @@ dtfs = get_df()
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     data = {
         "page": "Home page"
     }
-    return templates.TemplateResponse("index.html", {"request": request, "data": data})
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
 @app.get("/page/{page_name}", response_class=HTMLResponse)
@@ -60,7 +61,7 @@ async def page(request: Request, page_name: str):
     data = {
         "page": page_name
     }
-    return templates.TemplateResponse("index.html", {"request": request, "data": data})
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
 
