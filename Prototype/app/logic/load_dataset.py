65aaec38c16fd60e3e4aca2124d37b3c3bbc9303
AngelinaEiras <angie.eiras@gmail.com>
2023-07-07 09:25:33 +0100

    file cleaning

---
+++ Prototype/app/logic/load_dataset.py
@@ -0,0 +1,47 @@
+import openpyxl
+from pydantic import dataclasses
+import pandas as pd
+
+@dataclasses.dataclass()
+class LoadDataframeFromExcell:
+    excell_path : str
+    sheet_name : str
+
+    def load_subdatasets(self) -> pd.DataFrame:
+        wb = openpyxl.load_workbook(self.excell_path)
+        ws = wb[self.sheet_name] # ws = wb.active # access the working sheet ws = wb['vgsales']
+
+        # List to store the sub-datasets
+        subdatasets = []
+
+        # Flag to identify the start of a sub-dataset
+        start_flag = False
+
+        # iterate over the rows of the worksheet
+        for row in ws.iter_rows(values_only=True):
+            # check if the first cell starts with 'A'
+            if row[0] and str(row[0]).startswith('A'):
+                # set the start flag to True
+                start_flag = True
+                # create a new list to store the sub-dataset
+                subdataset = [row]
+            # check if the first cell starts with 'H'
+            elif row[0] and str(row[0]).startswith('H'):
+                # set the start flag to False
+                start_flag = False
+                # add the last row to the sub-dataset
+                subdataset.append(row)
+                # add the sub-dataset to the list
+                subdatasets.append(subdataset)
+            # add the row to the sub-dataset if the start flag is True
+            elif start_flag:
+                subdataset.append(row)
+
+        subdatasets = pd.DataFrame(subdatasets[0])
+        return subdatasets
+
+# Example usage
+#exxcell_path = "/home/angelina/Desktop/LabReport/src/file_manager/tests/artifacts/20230301_PB_triton_2.xlsx"
+#sheet_name = "Sheet1"
+#filename = "/home/angelina/Desktop/LabReport/src/file_manager/tests/artifacts/20230224_triton_VVB.xlsx"
+#sheet_name = "Plate 1 - Sheet1 (2)"
---
+++ Prototype/app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.library.helpers import *
+from app.routers import twoforms, unsplash, accordion
+from app.logic.load_dataset import LoadDataframeFromExcell
+import pandas as pd
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(unsplash.router)
+app.include_router(twoforms.router)
+app.include_router(accordion.router)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = openfile("home.md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def show_page(request: Request, page_name: str):
+    data = openfile(page_name+".md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+@app.get("/table/show", response_class=HTMLResponse)
+async def show_page(request: Request):
+    df = LoadDataframeFromExcell("/home/angelina/Desktop/LabReport/src/file_manager/tests/artifacts/20230301_PB_triton_2.xlsx", "Sheet1").load_subdatasets()
+    data = openfile("table_show.md")
+    return templates.TemplateResponse("table_page.html", {"request": request, "data": data, "dataframe": df})
