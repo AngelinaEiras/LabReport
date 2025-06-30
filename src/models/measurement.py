397c3c8454542d966525e9c6563dca0baabe6912
AngelinaEiras <angie.eiras@gmail.com>
2025-06-30 14:23:46 +0100

    improving the code flow

--- src/models/measurement.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class Measurement(BaseModel):
-    value : float
-    unit: str
-    timestamp: datetime
--- src/models/well.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-from src.models.measurement import Measurement
-
-class Well(BaseModel):
-    position: tuple[int, int]
-    measures: List[Measurement]
